d48970a6fca0980939c5ee360f826ae4a11d408d
Zarar Afzal <112646356+zararafzal@users.noreply.github.com>
2023-11-21 22:40:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
-import gradio as gr
+import streamlit as st
+from transformers import pipeline
 
-gr.load("models/michellejieli/emotion_text_classifier").launch()
+classifier = pipeline("text-classification", model="michellejieli/emotion_text_classifier")
+
+def classify_text(text):
+    result = classifier(text)[0]
+    return result["label"]
+
+st.text_input("Enter text here:", key="text")
+if st.button("Classify"):
+    result = classify_text(st.session_state.text)
+    st.write(result)
